56ddc57d7e0c27189bb4ff4472fe89dd78c0bbc2
EmmanuelBarboza <barbozaprogra18@gmail.com>
2025-01-10 22:40:58 -0600

    Added efect when enemy gets hit

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -13,12 +13,16 @@ extends CharacterBody2D
 		if health <= 0:
 			die()
 
+var squash_and_stretch := 1.0:
+	set(value):
+		squash_and_stretch = value
+		var _negative = 1.0 + (1.0 - squash_and_stretch)
+		animated_sprite.scale = Vector2(1, squash_and_stretch)
+
 func _ready() -> void:
 	pass
 
-
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	movement()
 	
 	move_and_slide()
@@ -47,8 +51,14 @@ func die() -> void:
 	set_process(false)
 	queue_free()
 
+func do_squash_and_stretch(value: float, duration: float = 0.1):
+	var tween = create_tween()
+	tween.tween_property(self, "squash_and_stretch", value, duration)
+	tween.tween_property(self, "squash_and_stretch", 1.0, duration * 1.8)
+
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	#Para cuando un proyectil toque la hitbox del enemigo
 	if area is Projectile:
 		area.queue_free()
+		do_squash_and_stretch(1.4,0.08)
 		health -= area.base_damage
